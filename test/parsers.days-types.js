36d5f6b4c240c5d5961abe75abd976d8f4673e3d
luski1 <l.godlewski@live.com>
2015-01-05 10:44:08 +0000

    refactor unit test for day types parser

--- test/parsers.days-types.js
+++ test/parsers.days-types.js
@@ -5,93 +5,34 @@
 var should = require('should'),
     parseDaysTypes = require('../src/parsers/days-types.js'),
     fs = require('fs'),
-    expectedDaysTypes = [{
-        "code": "D1",
-        "name": "PONIEDZIAŁEK"
-    }, {
-        "code": "D2",
-        "name": "WTOREK"
-    }, {
-        "code": "D3",
-        "name": "ŚRODA"
-    }, {
-        "code": "D4",
-        "name": "CZWARTEK"
-    }, {
-        "code": "D5",
-        "name": "PIĄTEK"
-    }, {
-        "code": "D6",
-        "name": "SOBOTA"
-    }, {
-        "code": "D7",
-        "name": "NIEDZIELA"
-    }, {
-        "code": "DP",
-        "name": "DZIEŃ POWSZEDNI"
-    }, {
-        "code": "DS",
-        "name": "ŚWIĘTO"
-    }, {
-        "code": "SB",
-        "name": "SOBOTA"
-    }, {
-        "code": "N1",
-        "name": "NOC PONIEDZIAŁEK/WTOREK"
-    }, {
-        "code": "N2",
-        "name": "NOC WTOREK/ŚRODA"
-    }, {
-        "code": "N3",
-        "name": "NOC ŚRODA/CZWARTEK"
-    }, {
-        "code": "N4",
-        "name": "NOC CZWARTEK/PIĄTEK"
-    }, {
-        "code": "N5",
-        "name": "NOC PIĄTEK/SOBOTA"
-    }, {
-        "code": "N6",
-        "name": "NOC SOBOTA/NIEDZIELA"
-    }, {
-        "code": "N7",
-        "name": "NOC NIEDZIELA/PONIEDZIAŁEK"
-    }, {
-        "code": "NS",
-        "name": "ROZKŁAD NOCNY"
-    }, {
-        "code": "NP",
-        "name": "ROZKŁAD WEEKENDOWY"
-    }, {
-        "code": "NO",
-        "name": "ROZKŁAD NOCNY"
-    }, {
-        "code": "TS",
-        "name": "ŚWIĘTO"
-    }, {
-        "code": "EX",
-        "name": "ROZKŁAD SPEC."
-    }, {
-        "code": "S1",
-        "name": "ROZKŁAD SPEC."
-    }, {
-        "code": "S2",
-        "name": "ROZKŁAD SPEC."
-    }, {
-        "code": "SR",
-        "name": "SOBOTA ROBOCZA"
-    }, {
-        "code": "WS",
-        "name": "WOLNA SOBOTA"
-    }];
+    Promise = require('promise');
 
 describe('downloadTests', function() {
     it('should parse proper days types', function(done) {
-        fs.readFile(__dirname + '/resource/days-types-content.txt', 'utf8', function(err, data) {
-            if (err) throw err;
-
-            parseDaysTypes(data).should.be.eql(expectedDaysTypes);
-            done();
-        });
+        
+        var expectedDaysTypes, daysTypes;
+        
+        getFileContent('/resource/days-types-content.output')
+        .then(function(data) {
+            expectedDaysTypes = JSON.parse(data);
+            return getFileContent('/resource/days-types-content.input');
+        })
+        .then(function(data) {
+            daysTypes = parseDaysTypes(data);
+            daysTypes.should.be.eql(expectedDaysTypes);
+        })
+        .then(done, done);
     });
 });
+
+function getFileContent(path) {
+    return new Promise(function (resolve, reject) {
+        fs.readFile(__dirname + path, 'utf8', function(err, data) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data);
+        });
+    });
+}
